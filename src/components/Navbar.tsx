7adfe49a216b53412fee04422ba33343d0149c27
kirubakaran <72642390+kirubakaran2@users.noreply.github.com>
2025-06-15 16:24:36 +0530

    commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Menu, X } from "lucide-react";
 import { Button } from "./ui/button";
-import logo from "../../public/logo.png";
+import logo from "../../public/logotransparent.png";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
